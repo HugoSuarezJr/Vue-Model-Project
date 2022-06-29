5eeb240fdb9eca65eb5fbee2f2d77a2fcec19c55
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-06-28 21:59:26 -0400

    named slots

--- js/components/App.js
+++ js/components/App.js
@@ -1,9 +1,32 @@
 import Assignments from "./Assignments.js";
+import Panel from "./Panel.js";
 
 export default {
-    components: {Assignments},
+    components: {Assignments, Panel},
 
      template: `
-        <assignments></assignments>
+         <div class="grid gap-6">
+            <assignments></assignments>
+
+            <panel>
+               default slot here?
+            </panel>
+
+            <panel>
+               <template v-slot:heading>heading</template>
+               default slot here?
+            </panel>
+
+            <panel theme="light">
+               <template v-slot:heading>
+                  Just a heading
+               </template>
+               Random Text for default
+
+               <template v-slot:footer>
+                  Just a footer
+               </template>
+            </panel>
+         </div>
      `
 };
